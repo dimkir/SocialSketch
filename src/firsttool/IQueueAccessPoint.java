6ad4fb35a45260ad3862f36a85d869bf5049fdec
Ftp User <ftp_user@inbox.lv>
2013-08-22 14:06:32 +0100

    adds IQueueAccessPoint interface

---
+++ src/firsttool/IQueueAccessPoint.java
@@ -0,0 +1,13 @@
+package firsttool;
+/**
+ * This interface functions provide points of accessing
+ * queue of messages.
+ */
+public interface IQueueAccessPoint 
+{
+     /**
+      * Returns next element in the queue if available.
+      * @return 
+      */
+      AbstractTweet getNextOrNull();
+}
