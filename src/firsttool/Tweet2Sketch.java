2effaa31bf62062268a1c20ab5ec08a12fc3a834
Ftp User <ftp_user@inbox.lv>
2013-09-16 17:46:03 +0100

    introduces "ISketchModder" interface and object. Now tool is dependent on this interface to create the "modder of sketches". In our understanding "modder" is something which helps "modding" sketches. (eg. sharing/fetching them)

--- src/firsttool/Tweet2Sketch.java
+++ src/firsttool/Tweet2Sketch.java
@@ -1,6 +1,9 @@
 package firsttool;
 
-import firsttool.EasyFrame.IOnTweetPickListener;
+import firsttool.ISketchModder.IOnModderActionListener;
+import firsttool.tweetqueue.AbstractTweet;
+import firsttool.ISketchModder.ModderAction;
+import javax.swing.JOptionPane;
 import processing.app.Editor;
 import processing.app.tools.Tool;
 
@@ -31,12 +34,16 @@ import processing.app.tools.Tool;
  * Boston, MA  02111-1307  USA
  */
 public class Tweet2Sketch  
-implements Tool,  IOnTweetPickListener
+implements Tool
 {
+
+
+    
+    
     Editor mEditor;
 
     private boolean mInitWasCalledAlredy = false;
-    private EasyFrame mEasyFrame;
+    private ISketchModder mSketchModder;
     
     @Override
     public void init(Editor editor) {
@@ -54,24 +61,16 @@ implements Tool,  IOnTweetPickListener
     @Override
     public void run() {
 //        writeLn("Now executing run() method. Has run() been called already?: " + mRunWasCalledAlredy);
-        if ( mEasyFrame == null) {
-            mEasyFrame = new EasyFrame();
-            mEasyFrame.setOnTweetPickListener(this);
+        if ( mSketchModder == null) {
+                mSketchModder = initModder(); // and adds listeners
         }
-        mEasyFrame.setVisible(true); // hopefull this one is not blocking.
+        
+        mSketchModder.setVisible(true); // hopefull this one is not blocking.
+        
+        
     }
     
 
-    /**
-     * This happens when someone selects a tweet.
-     * @param tweet
-     * @param easyFrame 
-     */
-    @Override
-    public void onTweetPickListener(AbstractTweet tweet, EasyFrame easyFrame) {
-        String tweetText = tweet.getText();
-        mEditor.insertText(tweetText);
-    }    
 
     @Override
     public String getMenuTitle() {
@@ -94,5 +93,57 @@ implements Tool,  IOnTweetPickListener
     public static void main(String[] args) {
         // TODO code application logic here
         // some testing? or actually we can't run it standalone??
-    }    
+    }
+
+    /**
+     * Just instantiates tweet UI (retrieving it from service)
+     * and sets up listeners.
+     * 
+     * @return 
+     */
+    private ISketchModder initModder() {
+        
+          ISketchModder  sketchModder = (ISketchModder) ServiceLocator.getSerivce(ServiceLocator.SVC_BASIC_SKETCH_MODDER);
+           
+          sketchModder.setOnModderActionListener(new MyOnModderActionListener());
+          
+          sketchModder.setSourceCodeFormatter(mEditor.createFormatter());
+          return sketchModder;
+    }
+    
+    
+
+    
+    private  class MyOnModderActionListener implements IOnModderActionListener, ISketchModder.ModderActionTypes
+    {
+
+        @Override
+        public void onModderAction(ISketchModder modder, ModderAction action) {
+            int actType = action.getActionType();
+            AbstractTweet tweet = action.getTweet();
+            String tweetText;
+            switch ( actType ){
+                case ACTION_CODE_EDITOR_INSERT_INTO:
+                    tweetText = tweet.getText();
+                    mEditor.insertText(tweetText);                    
+                    break;
+                case ACTION_CODE_EDITOR_REPLACE_WITH:
+//                    tweetText = tweet.getText();
+//                    mEditor.setText(tweetText);                    
+                    
+                    mEditor.setText(action.getSourceCode());      // this will return formatted source code.
+                                                                 // as this seems to be paramter for this action.
+                    
+                    break;
+                case ACTION_MODDER_REQUESTS_CODE:
+                    mSketchModder.sendSketchCode(mEditor.getText(), tweet);
+                    JOptionPane.showMessageDialog(mEditor, "Just sent code from PDE current window into tweetUI.");                    
+                    break;
+                
+            }
+        }
+    }
+
+        
+    
 }
