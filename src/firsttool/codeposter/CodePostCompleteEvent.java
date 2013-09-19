81edcb6c6b527fb7f09c5b602cc97767e40167f8
Ftp User <ftp_user@inbox.lv>
2013-09-19 09:23:02 +0100

    adds code posting via GIST and commenting with url.

--- src/firsttool/codeposter/CodePostCompleteEvent.java
+++ src/firsttool/codeposter/CodePostCompleteEvent.java
@@ -5,9 +5,33 @@ package firsttool.codeposter;
  * @author Dimitry Kireyenkov <dimitry@languagekings.com>
  */
 public class CodePostCompleteEvent {
+    
+    
+    private final boolean mOperationSuccess;
+    private final String mHtmlUrl;
+    private final String mErrorMessage;
+
+    /**
+     * Creates event. If event successful the param0 is the success url.
+     * If event is Unsuccessful param0 is the error message.
+     * @param param0
+     * @param successFlag 
+     */
+    CodePostCompleteEvent(String param0, boolean successFlag) {
+        // TODO: implement CodePostCompleteEvent
+        mOperationSuccess = successFlag;
+        if (  mOperationSuccess ){
+            mHtmlUrl = param0;
+            mErrorMessage = null;
+        }
+        else{
+            mErrorMessage = param0;
+            mHtmlUrl = null;
+        }
+    }
 
     public String getUrlString() {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mHtmlUrl;
     }
     
     
@@ -16,7 +40,7 @@ public class CodePostCompleteEvent {
      * @return 
      */
     public boolean isSuccessful(){
-        return false;
+        return mOperationSuccess;
     }
     
     
--- src/firsttool/codeposter/CodePoster.java
+++ src/firsttool/codeposter/CodePoster.java
@@ -1,7 +1,15 @@
 package firsttool.codeposter;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.eclipse.egit.github.core.Authorization;
+import org.eclipse.egit.github.core.Gist;
+import org.eclipse.egit.github.core.GistFile;
+import org.eclipse.egit.github.core.service.GistService;
+import org.eclipse.egit.github.core.service.OAuthService;
 
 /**
  * Provides API to post code online. 
@@ -12,6 +20,63 @@ import java.util.TimerTask;
  * @author Dimitry Kireyenkov <dimitry@languagekings.com>
  */
 public class CodePoster {
+    private final GistService mGistService;
+    private boolean C_IS_GIST_PUBLIC  = true; // makes all gists be visible on my gist-profile.
+                                            // otherwise they're only visible for people with link
+    
+    
+    /**
+     * Initializes Code Poster.
+     * 
+     * Incuim: connects to GITHUB and attempts to authorize user.
+     * @throws IOException 
+     */
+    public CodePoster() throws IOException{
+		OAuthService oauthService = new OAuthService();
+
+		// Replace with actual login and password
+		oauthService.getClient().setCredentials("dimkir", "lifebook777");
+
+		// Create authorization with 'gist' scope only
+		Authorization auth = new Authorization();
+		auth.setScopes(Arrays.asList("gist"));
+		auth = oauthService.createAuthorization(auth);
+
+		// Create Gist service configured with OAuth2 token
+		mGistService = new GistService();
+		mGistService.getClient().setOAuth2Token(auth.getToken());
+
+     
+    }
+    
+    
+    /**
+     * This method attempts to post gist and returns gist URI if successful.
+     * 
+     * @param contents
+     * @param name
+     * @return
+     * @throws IOException 
+     */
+    private String postGist(String contents, String name) throws IOException{
+		// Create Gist
+		Gist gist = new Gist();
+		gist.setPublic(C_IS_GIST_PUBLIC);
+                
+		gist.setDescription("Created using Sketch2Tweet tool");
+		GistFile file = new GistFile();
+		file.setContent(contents);
+		file.setFilename(name);
+                
+		gist.setFiles(Collections.singletonMap(file.getFilename(), file));
+		gist = mGistService.createGist(gist);
+                mGistService.createComment(gist.getId(), 
+                   "This is comment which can show image of the sketch ![imge](http://s13.postimg.org/6snibyalj/screenshot_155.png)");
+                
+		System.out.println("Created Gist at " + gist.getHtmlUrl());              
+                return gist.getHtmlUrl();
+    }
+           
 
     /**
      * Posts code to some "pasting service" and returns url.
@@ -21,20 +86,25 @@ public class CodePoster {
      * @param text
      * @param code to be executed on the same thread as it was called... but how to achieve it???
      */
-    public void postCode(String text, final ICodePostComplete iCodePostComplete) {
+    public void postCode(final String text, final ICodePostComplete iCodePostComplete) {
         //throw new UnsupportedOperationException("Not yet implemented");
         // ?? can I use timer?
         // TODO: this shouldn't be called immediately, but after some time.
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-
+        Thread thread = new Thread(){
             @Override
             public void run() {
-                iCodePostComplete.onCodePostComplete(new CodePostCompleteEvent());
+                try {
+                    String gistUrl = postGist(text, "Sketch code");
+                    CodePostCompleteEvent completeEvent = new CodePostCompleteEvent(gistUrl, true /* success flag */);
+                    iCodePostComplete.onCodePostComplete(completeEvent);
+                } catch (IOException ex) {
+                    Logger.getLogger(CodePoster.class.getName()).log(Level.SEVERE, null, ex);
+                    iCodePostComplete.onCodePostComplete(new CodePostCompleteEvent("Error message", false /* success flag */));
+                }
                 //throw new UnsupportedOperationException("Not supported yet.");
             }
-        }, 
-                1000);
+        };
+        thread.start();
         
     }
     
