ba6c6b2237b5f56bca7fc9b02be50a4d0e859ca6
Ftp User i3 Wingit <ftp_user@inbox.lv>
2013-11-10 23:01:53 +0000

    adds interface which will help print error messages to console

---
+++ src/org/socialsketch/tool/IDebugPrint.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.socialsketch.tool;
+
+/**
+ *
+ * @author Dimitry Kireyenkov <dimitry@languagekings.com>
+ */
+public interface IDebugPrint {
+    void writeLn(String s);
+}
