a94d1a07ae7ca3cbd5358c71e85a21fbd7d5fb60
Ftp User i3 Wingit <ftp_user@inbox.lv>
2013-11-12 13:40:59 +0000

    draf version of the adatper

---
+++ src/org/socialsketch/tool/SketchAdapter.java
@@ -0,0 +1,133 @@
+package org.socialsketch.tool;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.socialsketch.tool.codeposter.ICodePostAdapter;
+import processing.app.Sketch;
+import processing.app.SketchCode;
+
+/**
+ * This is adapter to adapt sketch files to the format which SketchAdapter understands.
+ * 
+ * @author Dimitry Kireyenkov <dimitry@languagekings.com>
+ */
+public class SketchAdapter  implements ICodePostAdapter
+{
+
+    /**
+     * If this is not null, we will be "updating" existing one,
+     * as opposed to saving it as new.
+     * 
+     */
+    private String mPeviousGistId = null;
+    
+    private Map<String, String> mPrograms = new HashMap<String,String>();
+    private List<String> mFileNames = new ArrayList<String>();
+    
+    /**
+     * This is signature inside of the main code, which shows the reference to the
+     * gist.
+     * 
+     */
+    private static final String C_GIST_SIGNATURE = "//#GIST:";
+    private String mDescription;
+    
+    /**
+     * Initializes adapter with current state of the sketch.
+     * 
+     * @param sketch 
+     */
+    public SketchAdapter(Sketch sketch) {
+        int cc = sketch.getCodeCount();
+        
+        mPeviousGistId =  parseMainProgramForId(    sketch.getMainProgram() );
+        
+        mDescription = "Processing sketch : " + sketch.getName();
+        
+        for(int  i = 0 ; i < cc ; i++){
+            SketchCode code = sketch.getCode(i);
+            // what can we do with this code?
+            String fileName = code.getFile().getName();
+            String program = code.getProgram();
+            mPrograms.put(fileName, program);
+            mFileNames.add(fileName);
+        }
+    }
+
+    
+    
+    /**
+     * Defensive copy is returned. 
+     * 
+     * @return 
+     */
+    @Override
+    public String[] getFileNames() {
+        String[] fns = new String[mFileNames.size()];
+        mFileNames.toArray(fns);
+        return fns;
+    }
+
+    @Override
+    public int getFileCount() {
+        return mFileNames.size();
+    }
+
+    @Override
+    public String getFileContentsByIndex(int idx) {
+        String key = mFileNames.get(idx);
+        return mPrograms.get(key);
+    }
+
+    @Override
+    public String getFileContentsByName(String name) {
+        return mPrograms.get(name);
+    }
+
+    @Override
+    public String getPreviousPostId() {
+        return mPeviousGistId;
+    }
+
+    /**
+     * Parses main program for comment containing gist and then
+     * returns it, when it is available! 
+     * 
+     * @param mainProgram
+     * @return 
+     */
+    private static String parseMainProgramForId(String mainProgram) {
+        
+        int idx = mainProgram.indexOf(C_GIST_SIGNATURE);
+        if ( idx == -1){
+            return null; // there's no element found
+        }
+        
+        // calcualte index following signature.
+        int idxFollowingSignature = idx + C_GIST_SIGNATURE.length();
+        
+        StringBuilder sb = new StringBuilder();
+        while (  Character.isDigit(mainProgram.charAt(idxFollowingSignature)) ){
+            sb.append(mainProgram.charAt(idxFollowingSignature++));
+        }
+        //int endIdx = mainProgram.indexOf(' ', idxFollowingSignature);
+        // or maybe we should only count numbers.
+        //String extractedId = mainProgram.substring(idx + C_GIST_SIGNATURE.length(), endIdx );
+        
+        return sb.length() == 0 ? null : sb.toString();
+    }
+
+    @Override
+    public String getDescription() {
+        return mDescription;
+    }
+
+    @Override
+    public String getFileNameByIndex(int i) {
+        return mFileNames.get(i);
+    }
+    
+    
+}
